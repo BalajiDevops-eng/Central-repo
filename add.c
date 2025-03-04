46a9b9781b9b01b2759985e95090c21bb3ac145f
Balaji.S <balajishesha06@gmail.com>
2025-03-04 05:44:56 +0000

    coreection

--- add.c
+++ add.c
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # Get the two numbers from the user
-echo "Enter the first number: "
+echo "Enter the first number:"
 read num1
-echo "Enter the second number: "
+
+echo "Enter the second number:"
 read num2
 
 # Calculate the sum
